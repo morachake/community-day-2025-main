279ad0b2ea02dcd0d2180a4457f05f83ad443e20
benmuuo <benmuuo.bm@gmail.com>
2026-04-21 07:26:14 +0300

    Fix load-time page jump behavior

--- js/badge/main.js
+++ js/badge/main.js
@@ -161,17 +161,6 @@
 	);
 
 	/**
-	 * Scroll with ofset on page load with hash links in the url
-	 */
-	window.addEventListener('load', () => {
-		if (window.location.hash) {
-			if (select(window.location.hash)) {
-				scrollto(window.location.hash);
-			}
-		}
-	});
-
-	/**
 	 * Initiate glightbox
 	 */
 	const glightbox = GLightbox({
